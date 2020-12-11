391e6861f5af31b984fb85a6d46f97e3d8457a50
AdamSpannbauer <spannbaueradam@gmail.com>
2020-12-11 09:59:11 -0500

    add sliders to adjust narms and njoints

--- sketch.js
+++ sketch.js
@@ -1,33 +1,44 @@
 // eslint-disable-next-line import/extensions
 import armClass from './src/arm.js';
 
-const canvasW = 512;
-const canvasH = 512;
+const learningBGColor = [250, 250, 250];
+const chasingBGColor = [200, 200, 200];
 
-const arms = [];
-const nArms = 1;
+let resetButton;
+let nJointsSlider;
+let nArmsSlider;
+
+let arms;
 
 let learning = true;
-const learningFrames = 1000;
+const learningFrames = 500;
 
 const armPlacementRadius = 0;
 
-function setup() {
-  createCanvas(canvasW, canvasH);
+let t = 0;
 
-  for (let i = 0; i < nArms; i += 1) {
-    const angle = i * (TWO_PI / nArms);
+function reset() {
+  background(learningBGColor);
+
+  t = 0;
+  learning = true;
+
+  arms = [];
+  for (let i = 0; i < nArmsSlider.value(); i += 1) {
+    const angle = i * (TWO_PI / nArmsSlider.value());
     const x = cos(angle) * armPlacementRadius;
     const y = sin(angle) * armPlacementRadius;
 
     const lens = [];
     const sws = [];
-    for (let j = 0; j < round(random(1, 5)); j += 1) {
+    for (let j = 0; j < nJointsSlider.value(); j += 1) {
       const minLen = 50 - j * 10;
       const maxLen = 80 - j * 10;
       lens.push(random(minLen, maxLen));
 
-      sws.push(10 - j * 2);
+      let sw = 10 - j * 2;
+      if (sw <= 0) sw = 1;
+      sws.push(sw);
     }
 
     lens.push(10);
@@ -39,14 +50,35 @@ function setup() {
   }
 }
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  nArmsSlider = createSlider(1, 3, 1, 1);
+  nArmsSlider.position(10, 30);
+  nJointsSlider = createSlider(2, 6, int(random(2, 7)), 1);
+  nJointsSlider.position(10, 70);
+
+  resetButton = createButton('Reset');
+  resetButton.position(10, 95);
+  resetButton.mouseClicked(reset);
+
+  reset();
+}
+
 function draw() {
-  if (frameCount > learningFrames) {
+  t += 1;
+  if (t > learningFrames) {
     learning = false;
   }
 
   if (!learning) {
-    background(200);
+    background(chasingBGColor);
   }
+  noStroke();
+  fill(0);
+  text('Number of Arms', 10, 25);
+  text('Number of Joints', 10, 65);
+
   translate(width / 2, height / 2);
 
   noStroke();
